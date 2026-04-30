8e8f618594265c8078bac70cbd28c457c17b5722
Guilherme <142947544+Guims0@users.noreply.github.com>
2026-04-30 16:25:18 -0300

    Novo projeto

---
+++ src/test/java/estopa/agenda_facil/AgendaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package estopa.agenda_facil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
