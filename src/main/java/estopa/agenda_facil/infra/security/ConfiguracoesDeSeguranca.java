408fce469403fe47d44426074369eb669b40eb7d
Guilherme <142947544+Guims0@users.noreply.github.com>
2026-05-13 16:20:10 -0300

    Configuraçao pro jwt

---
+++ src/main/java/estopa/agenda_facil/infra/security/ConfiguracoesDeSeguranca.java
@@ -0,0 +1,53 @@
+package estopa.agenda_facil.infra.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class ConfiguracoesDeSeguranca {
+
+    private final FiltroDeSeguranca filtroDeSeguranca;
+
+    @Bean
+    public SecurityFilterChain configuracaoDeFiltros(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+
+                    req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                    req.requestMatchers(HttpMethod.POST, "/clientes").permitAll();
+                    req.requestMatchers(HttpMethod.POST, "/estabelecimentos").permitAll();
+
+                    req.anyRequest().authenticated();
+                })
+
+                .addFilterBefore(filtroDeSeguranca, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager gerenciadorDeAutenticacao(AuthenticationConfiguration configuracao) throws Exception {
+
+        return configuracao.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder encriptadorDeSenha() {
+
+        return new BCryptPasswordEncoder();
+    }
+}
