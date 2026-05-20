944529fde502bd1d7b89724c4f37b8581438e95b
Guilherme <142947544+Guims0@users.noreply.github.com>
2026-05-20 18:05:42 -0300

    Implementacao do swagger para documentacao e atualizacao da classe configuracoes de seguranca

--- src/main/java/estopa/agenda_facil/infra/security/ConfiguracoesDeSeguranca.java
+++ src/main/java/estopa/agenda_facil/infra/security/ConfiguracoesDeSeguranca.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class ConfiguracoesDeSeguranca {
 
@@ -27,27 +29,23 @@ public class ConfiguracoesDeSeguranca {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
-
                     req.requestMatchers(HttpMethod.POST, "/login").permitAll();
                     req.requestMatchers(HttpMethod.POST, "/clientes").permitAll();
                     req.requestMatchers(HttpMethod.POST, "/estabelecimentos").permitAll();
-
+                    req.requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll();
                     req.anyRequest().authenticated();
                 })
-
                 .addFilterBefore(filtroDeSeguranca, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
     @Bean
     public AuthenticationManager gerenciadorDeAutenticacao(AuthenticationConfiguration configuracao) throws Exception {
-
         return configuracao.getAuthenticationManager();
     }
 
     @Bean
     public PasswordEncoder encriptadorDeSenha() {
-
         return new BCryptPasswordEncoder();
     }
 }
